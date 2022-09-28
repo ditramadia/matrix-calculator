150210379d664a690ac050e13bba5e64b17af4e0
ditramadia <ditrarizqaamadia@gmail.com>
2022-09-28 14:25:47 +0700

    Add Arithmetic Operation Method in Matrix Class

--- src/algeo01/data/Matrix.java
+++ src/algeo01/data/Matrix.java
@@ -20,6 +20,21 @@ public class Matrix {
             }
         }
     }
+    // Create Matrix form Augmented Array
+    public Matrix(double[][] tab){
+        this.nRow = tab.length;
+        this.nCol = tab[0].length;
+        this.tab = new double[this.nRow][this.nCol];
+        for (int i = 0; i < this.nRow; i++){
+            for (int j = 0; j < this.nCol; j++){
+                this.tab[i][j] = tab[i][j];
+            }
+        }
+    }
+    // Copy Matrix
+    public Matrix(Matrix M){
+        this(M.tab);
+    }
 
     // METHODS //
     // Getter and Setter Methods
@@ -61,10 +76,54 @@ public class Matrix {
         }
     }
 
+    // Arithmetic Operation
+    public Matrix addMatrix(Matrix M){
+        Matrix result = new Matrix(this.nRow, this.nCol);
+        for(int i = 0; i < this.nRow; i++){
+            for(int j = 0; j < this.nCol; j++){
+                result.tab[i][j] = getElmt(i, j) + M.getElmt(i, j);
+            }
+        }
+        return result;
+    }
+    public Matrix subtractMatrix(Matrix M){
+        Matrix result = new Matrix(this.nRow, this.nCol);
+        for(int i = 0; i < result.nRow; i++){
+            for(int j = 0; j < result.nCol; j++){
+                result.tab[i][j] = getElmt(i, j) - M.getElmt(i, j);
+            }
+        }
+        return result;
+    }
+    public Matrix multiplyMatrix(Matrix M){
+        Matrix result = new Matrix(this.nRow, M.nCol);
+        for(int i = 0; i < result.nRow; i++){
+            for(int j = 0; j < result.nCol; j++){
+                for(int k = 0; k < M.nRow; k++){
+                    result.tab[i][j] += this.tab[i][k] * M.tab[k][j];
+                }
+            }
+        }
+        return result;
+    }
+    public Matrix multiplyByConst(double x){
+        Matrix result = new Matrix(this.nRow, this.nCol);
+        for(int i = 0; i < result.nRow; i++){
+            for(int j = 0; j < result.nCol; j++){
+                result.tab[i][j] = this.tab[i][j] * x;
+            }
+        }
+        return result;
+    }
+
     // Test Field
     public static void main(String[] args) {
-        Matrix matrix1 = new Matrix(3, 3);
+        Matrix matrix1 = new Matrix(4, 3);
+//        Matrix matrix2 = new Matrix(3, 4);
+        Matrix matrix3 = new Matrix(4, 4);
         matrix1.readMatrix();
-        matrix1.displayMatrix();
+//        matrix2.readMatrix();
+        matrix3 = matrix1.multiplyByConst(4);
+        matrix3.displayMatrix();
     }
 }
